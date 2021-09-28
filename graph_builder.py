4d81448c82e9f4c5c84199733f11f054676f1c38
mwappner <marcoswappner@gmail.com>
2021-09-28 09:08:52 -0300

    Documentación de las funciones

--- graph_builder.py
+++ graph_builder.py
@@ -2,13 +2,33 @@ import osmnx as ox
 from random import randrange
 
 class GraphBuilder:
+    """
+    Builds a graph of a given city with all the needed data (nodes, POIs, distance between those
+    measured in time units given a transport mode). It also provides a few useful methods to handle
+    said graph.
+    """
+
     def __init__(self, place, transportation_mode, travel_speed):
+        """
+        Initialized an empty graph. It's gonna use the following parameters to actually
+        process it:
+
+        place : str
+            city name
+        transportation_mode : str
+            transportation mode code
+        travel_speed : float
+            in km/h
+        """
         self.place = place
         self.transportation_mode = transportation_mode
         self.travel_speed = travel_speed # in km/h
         self.graph = None
 
     def initialize_graph(self):
+        """
+        Initialize graph by downloading it form OSM database with the adequate parameters.
+        """
         self.graph = ox.graph_from_place(
             self.place,
             network_type=self.transportation_mode
@@ -16,12 +36,19 @@ class GraphBuilder:
         return self.graph
 
     def calculate_centroid(self):
+        """
+        Calculates centroid of the whole graph.
+        """
         gdf_nodes = ox.graph_to_gdfs(self.graph, edges=False)
         x, y = gdf_nodes["geometry"].unary_union.centroid.xy
         center_node = ox.distance.nearest_nodes(self.graph, x[0], y[0])
         return center_node
 
     def initialize_projected_graph(self):
+        """
+        Maps nodes in the graph to a cartersian coordiante system that can actually be 
+        represented in an image with some sence of geographic distribution of the nodes.
+        """
         self.projected_graph = ox.project_graph(self.graph)
         meters_per_minute = self.travel_speed * 1000 / 60  # km per hour to m per minute
         
@@ -31,11 +58,20 @@ class GraphBuilder:
         return self.projected_graph
 
     def get_random_node(self):
+        """
+        Returns a random node in the graph. Useful for testing.
+        """
         nodes = list(self.graph.nodes)
         random_index = randrange(len(nodes))
         return nodes[random_index]
         
     def nearest_nodes_to(self, coordinates_list):
+        """
+        A way to convert coordiantes into nodes, by finding the nearest representation
+        of said coordiante in the available nodes. 
+        See https://osmnx.readthedocs.io/en/stable/osmnx.html#osmnx.distance.nearest_nodes
+        for more.
+        """
         longitudes = [coordinate[0] for coordinate in coordinates_list]
         latitudes = [coordinate[1] for coordinate in coordinates_list]
         return ox.distance.nearest_nodes(self.graph, longitudes, latitudes)
--- park_coordinates.py
+++ park_coordinates.py
@@ -1,19 +1,34 @@
 import json, os
 
 class ParkCoordinates:
+    """
+    Reads database as given in filename. Database contains coordiantes of the centroids of points of interest.
+    """
     def __init__(self):
         self._dataset = {}
         
     def load_park_coordinates_from(self, filename):
+        """
+        Loads up filename nto a dictionary keyed with coordiante IDs and coordiantes as values.
+        """
         with(open("data/parquesCorteArea2_5perc_CENTROIDES.geojson")) as f:
             self._dataset = json.loads(f.read())
         return self
             
     def coordinates_of(self, index):
+        """
+        Returns coordiantes of a given index (ID).
+        """
         return self.feature_list()[index]["geometry"]["coordinates"]
         
     def all_coordinates(self):
+        """
+        Returns a list of all coordiantes, ordered as indexed.
+        """
         return [self.coordinates_of(index) for index in range(len(self.feature_list()))]
     
     def feature_list(self):
+        """
+        Returns feature list of the dataset, features being for example parks.
+        """
         return self._dataset["features"]
